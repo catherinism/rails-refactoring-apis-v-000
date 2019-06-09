d7707900434d983ea03a55c81d2b8db9e3e34dec
catherinism <catcreates13@gmail.com>
2019-06-09 16:57:40 -0400

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -12,6 +12,13 @@ class GithubService
         {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
+
+    # user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # user_json = JSON.parse(user_response.body)
+    # session[:username] = user_json["login"]
+    #
+    # redirect_to '/'
+    #
   end
 
   def get_username
